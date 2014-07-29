4848a4c286b0a59fad5f4a48f5733dbcf192e26b
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-07-29 12:27:43 +0100

    Don't do the weird underscore thing
    * Also, don't keep printing the mahoosive output

--- tests/test_db_converter.py
+++ tests/test_db_converter.py
@@ -10,7 +10,6 @@ def output():
 		stdout=subprocess.PIPE
 	)
 	out = proc.communicate()[0]
-	print out
 	return out
 
 def test_varchar_sizes_are_normal():
@@ -20,14 +19,14 @@ def test_non_primary_and_non_unique_indexes_are_kept():
     assert 'CREATE INDEX "index_hits_on_mapping_id" ON "hits" ("mapping_id");' in output()
 
 def test_unique_indexes_are_kept():
-    _output = output()
-    assert '-- UNIQUE Indexes' in _output
-    assert 'CREATE UNIQUE INDEX "index_sites_on_site" ON sites ("abbr");' in _output
+    actual_output = output()
+    assert '-- UNIQUE Indexes' in actual_output
+    assert 'CREATE UNIQUE INDEX "index_sites_on_site" ON sites ("abbr");' in actual_output
 
 def test_defaults_are_kept_via_typecast():
-    _output = output()
-    assert "\"global_redirect_append_path\" int4 NOT NULL DEFAULT '0'" in _output
+    actual_output = output()
+    assert "\"global_redirect_append_path\" int4 NOT NULL DEFAULT '0'" in actual_output
     assert "ALTER TABLE \"sites\" ALTER COLUMN \"managed_by_transition\" DROP DEFAULT, "\
-           "ALTER COLUMN \"managed_by_transition\" TYPE boolean USING CAST(\"managed_by_transition\" as boolean);" in _output
-    assert "-- Defaults --" in _output
-    assert "ALTER TABLE \"sites\" ALTER COLUMN \"managed_by_transition\" SET DEFAULT 't';" in _output
+           "ALTER COLUMN \"managed_by_transition\" TYPE boolean USING CAST(\"managed_by_transition\" as boolean);" in actual_output
+    assert "-- Defaults --" in actual_output
+    assert "ALTER TABLE \"sites\" ALTER COLUMN \"managed_by_transition\" SET DEFAULT 't';" in actual_output
